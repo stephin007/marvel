8f9cc66d11ce5de813675d6ce8ad3a5bd175bcdc
LuckyPigeon <lucky90322@gmail.com>
2020-11-21 21:21:54 +0800

    Add Search Bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,14 @@
 import React, {Fragment} from 'react'
 
-const Search = () => {
+const Search = ({keyword, setKeyword}) => {
     return (
         <Fragment>
-            
+            <input 
+            class="search"
+            key="searchbox"
+            value={keyword}
+            placeholder={"Search hero"}
+            />
         </Fragment>
     )
 }
