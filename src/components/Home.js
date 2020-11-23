cfe29283da3b1d1f1115de90395cd7eed6dcb52d
stephin007 <stephinreji123@gmail.com>
2020-11-23 09:44:41 +0530

    Added first 100 characters to the search rop down list:octocat:

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,25 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 
 import Search from "./Search"
 import CardSection from "./cardSection"
 
+import {getCharacters} from "../api/Api"
+
 const Home = () => {
+    const [characters, setCharacters] = useState([])
+
+    useEffect(()=>{
+        getCharacters()
+        .then((output)=>{
+            setCharacters(output.data.results)
+            console.log(output.data.results)
+        })
+
+    },[])
+
     return (
         <Fragment>
-            <Search />
+            <Search characters={characters}/>
             <CardSection />
         </Fragment>
     )
