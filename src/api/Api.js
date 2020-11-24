610937e97aba23abe41367f0c7282e4fb62c6180
LuckyPigeon <lucky90322@gmail.com>
2020-11-25 02:57:19 +0800

    Add HASH and timestamp for API query

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,8 +1,9 @@
 const API_KEY = process.env.REACT_APP_MARVEL_API_KEY;
+const API_HASH = process.env.REACT_APP_MARVEL_API_HASH;
 
 // Get all the Characters
 export const getCharacters = () =>{
-    const URL = `https://gateway.marvel.com/v1/public/characters?limit=100&apikey=${API_KEY}`
+    const URL = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${API_KEY}&hash=${API_HASH}&limit=100`
     return fetch(URL)
     .then((response) =>response.json())
     .catch((error) => console.log("ERROR : ", error))
