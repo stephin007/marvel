8bb4258df0e72c145ed2da6eef42b8188fce2f6d
stephin007 <stephinreji123@gmail.com>
2020-11-24 05:51:40 +0530

    Added scroll to top fucntionality

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ const API_KEY = process.env.REACT_APP_MARVEL_API_KEY;
 
 // Get all the Characters
 export const getCharacters = () =>{
-    const URL = `https://gateway.marvel.com/v1/public/characters?limit=50&apikey=${API_KEY}`
+    const URL = `https://gateway.marvel.com/v1/public/characters?limit=100&apikey=${API_KEY}`
     return fetch(URL)
     .then((response) =>response.json())
     .catch((error) => console.log("ERROR : ", error))
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,67 @@
 import React, {Fragment, useState, useEffect} from 'react'
+import PropTypes from 'prop-types'
+import {useScrollTrigger, Fab, Zoom, Toolbar} from '@material-ui/core';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { makeStyles, createMuiTheme,ThemeProvider } from '@material-ui/core/styles';
+
 
 import Search from "./Search"
 import CardSection from "./cardSection"
 
 import {getCharacters} from "../api/Api"
 
-const Home = () => {
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#fdb82f",
+      },
+    //   secondary: {
+    //     main: '#11cb5f',
+    //   },
+    },
+  })
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+    children: PropTypes.element.isRequired,
+    window: PropTypes.func,
+  };
+
+const Home = (props) => {
     const [characters, setCharacters] = useState([])
 
     useEffect(()=>{
@@ -14,13 +70,20 @@ const Home = () => {
             setCharacters(output.data.results)
             console.log(output.data.results)
         })
-
     },[])
 
     return (
         <Fragment>
+            <Toolbar id="back-to-top-anchor" style={{marginTop: "-60px"}}/>
             <Search characters={characters}/>
             <CardSection />
+            <ThemeProvider theme={theme}>
+                <ScrollTop {...props}>
+                    <Fab color="primary" size="small" aria-label="scroll back to top">
+                    <KeyboardArrowUpIcon />
+                    </Fab>
+                </ScrollTop>
+            </ThemeProvider>    
         </Fragment>
     )
 }
