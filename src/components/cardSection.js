64ceab951cdee39adb94b2c22e565208586a11a8
LuckyPigeon <lucky90322@gmail.com>
2020-11-25 13:26:43 +0800

    List hero cards by API call

--- src/components/cardSection.js
+++ src/components/cardSection.js
@@ -1,39 +1,14 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import CardItem from "./CardItem"
-const CardSection = () => {
-    const [profiles] = useState([
-        {
-            id: 1009146,
-            image: "http://i.annihil.us/u/prod/marvel/i/mg/9/50/4ce18691cbf04/landscape_xlarge.jpg",
-            title: "Abomination (Emil Blonsky)",
-            description: "Formerly known as Emil Blonsky, a spy of Soviet Yugoslavian origin working for the KGB, the Abomination gained his powers after receiving a dose of gamma radiation similar to that which transformed Bruce Banner into the incredible Hulk."
-        },
-        {
-            id: 1016823,
-            image: "http://i.annihil.us/u/prod/marvel/i/mg/9/50/4ce18691cbf04/landscape_xlarge.jpg",
-            title: "Abomination (Ultimate)",
-            description: ""
-        },
-        {
-            id: 1016826,
-            image: "http://i.annihil.us/u/prod/marvel/i/mg/9/50/4ce18691cbf04/landscape_xlarge.jpg",
-            title: "Abomination (Ultimate)",
-            description: "Formerly known as Emil Blonsky, a spy of Soviet Yugoslavian origin working for the KGB, the Abomination gained his powers after receiving a dose of gamma radiation similar to that which transformed Bruce Banner into the incredible Hulk."
-        }
-    ])
-
-    const cards = (
-        profiles.map(profile => {
-            return ( 
-            <CardItem image={profile.image} title={profile.title} description={profile.description} key={profile.id} />
-            )
-        })
-    )
+const CardSection = ({heroes}) => {
+    console.log(heroes)
 
     return (
         <div className="container card-section" >
-            { cards }
+            { heroes.map(hero => (
+                <CardItem image={hero.thumbnail.path + "/landscape_xlarge.jpg"} title={hero.name} description={hero.description} key={hero.id} />
+            )) }
         </div>
     )
 }
