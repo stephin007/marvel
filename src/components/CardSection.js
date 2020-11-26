86bc59c5b92e17f15d86988b5d229df24f0cb8ca
stephin007 <stephinreji123@gmail.com>
2020-11-26 07:03:52 +0530

    Added Spinner gif and laodingState in the Card Section

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
 import CardItem from "./CardItem"
-const CardSection = ({heroes}) => {
-    console.log(heroes)
+import Spinner from "../assets/spinner.gif"
+
+const CardSection = ({heroes, loading}) => {
 
     return (
-        <div className="container card-section" >
-            { heroes.map(hero => (
-                <CardItem image={hero.thumbnail.path + "/landscape_xlarge.jpg"} title={hero.name} description={hero.description} key={hero.id} />
-            )) }
-        </div>
+    <Fragment>
+        {
+            loading === false 
+            ? (
+                <div className="container card-section" >
+                    { heroes.map(hero => (
+                        <CardItem image={hero.thumbnail.path + "/landscape_xlarge.jpg"} title={hero.name} description={hero.description} key={hero.id} />
+                    )) }
+                </div>
+            ) : (
+                <img src={Spinner} alt="Loading..." style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: "200", height: "200"}} />
+            )
+        }   
+    </Fragment>
     )
 }
 
