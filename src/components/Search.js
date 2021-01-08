9ac54c8038ffd1527cbd5156ca8bc0d54d03b31f
stephin007 <stephinreji123@gmail.com>
2021-01-08 10:40:38 +0530

    Commiting until next time

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
-const Search = ({characters, setCharacter}) => {
-    return (
-        <div className="container search">
-            <Autocomplete
-                id="combo-box-demo"
-                options={characters}
-                getOptionLabel={(option) => option.name}
-                style={{ width: "100%" }}
-                renderInput={(params) => <TextField {...params} id="standard-full-width" label="Search" placeholder="Search Heroes..." variant="outlined" />}
-                onChange={(e) => setCharacter(e.target.textContent)}
+const Search = ({ characters, setCharacter }) => {
+  return (
+    // <form noValidate autoComplete="off" className="container search">
+    //   <TextField
+    //     id="outlined-basic"
+    //     label="Search"
+    //     placeholder="Search Heroes..."
+    //     variant="outlined"
+    //     style={{ width: "100%" }}
+    //     onChange={(e) => setCharacter(e.target.textContent)}
+    //   />
+    // </form>
+    <div className="container search">
+        <Autocomplete
+            id="combo-box-demo"
+            options={characters}
+            getOptionLabel={(option) => option.name}
+            style={{ width: "100%" }}
+            renderInput={(params) => <TextField {...params} id="standard-full-width" label="Search" placeholder="Search Heroes..." variant="outlined" />}
+            onChange={(e) => setCharacter(e.target.textContent)}
             />
         </div>
-    )
-}
+  );
+};
 
-
-export default Search
+export default Search;
